91536ad0ed57c92f0603b521ddc7f0470bfac7cf
Miguel <obitosth@gmail.com>
2022-02-19 20:22:08 -0500

    frontend adminlte dia2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
-import React from "react";
-import Aside from "./components/Aside";
-import Content from "./components/Content";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Aside from "./components/Aside/Aside";
+import Content from "./components/Content/Content";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 //import "./../public/dist/css/adminlte.min.css";
+//import { Register } from "./components/Content/register";
 
 export default function App() {
   return (
-    <div>
-      <Header />
-      <Aside />
-      <Content />
-      <Footer />
+    <div className="app">
+      <Router>
+        <Header />
+        <Aside />
+        <Content />
+        {/* <Routes>
+          <Route path="/ingreso/registro">
+            <Register />
+          </Route>
+        </Routes> */}
+        <Footer />
+      </Router>
     </div>
   );
 }
