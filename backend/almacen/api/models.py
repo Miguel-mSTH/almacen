33113efc1f7174d40541d6ef680ef41dac48af9d
Miguel <obitosth@gmail.com>
2022-02-22 23:34:44 -0500

    backend crud ambiente

--- backend/almacen/api/models.py
+++ backend/almacen/api/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Categoria(models.Model):
-    nombre=models.CharField(max_length=100)
+    categoria_nombre=models.CharField(max_length=100)
 
     def __str__(self):
-        return self.nombre
+        return self.categoria_nombre
+
+class Ambiente(models.Model):
+    ambiente_nombre=models.CharField(max_length=100);
+    ambiente_ubicacion=models.CharField(max_length=200);
+    ambiente_tipo=models.IntegerField(default=1);
+
+    def __str__(self):
+        return self.ambiente_nombre
